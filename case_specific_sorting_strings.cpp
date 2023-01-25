dc2324d3fc28f68d2d907bccbad475cceb84bfa4
huggin <1171705+huggin@users.noreply.github.com>
2023-01-25 21:32:51 +0000

    Case-specific Sorting of Strings

---
+++ case_specific_sorting_strings.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to perform case-specific sorting of strings.
+    string caseSort(string str, int n)
+    {
+        // your code here
+        string up, lo;
+        for(char c : str) {
+            if (isupper(c)) {
+                up.push_back(c);
+            } else {
+                lo.push_back(c);
+            }
+        }
+        sort(begin(up), end(up));
+        sort(begin(lo), end(lo));
+        int i = 0, j = 0;
+        for(char& c : str) {
+            if (isupper(c)) {
+                c = up[i++];
+            } else {
+                c = lo[j++];
+            }
+        }
+        return str;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string str;
+		cin>>str;
+		Solution obj;
+		cout<<obj.caseSort (str, n)<<endl;
+	}
+}
+// } Driver Code Ends
