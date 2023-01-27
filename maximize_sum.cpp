077c1f727f95708d4746f84501e0cb805a1e4d72
huggin <rwangth@gmail.com>
2023-01-27 04:00:17 +0000

    Maximize sum(arr[i]*i) of an Array

---
+++ maximize_sum.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int Maximize(int a[],int n)
+    {
+        // Complete the function
+        const int M = 1e9+7;
+        sort(a, a+n);
+        long long ans = 0;
+        for(long long i=n-1; i>=0; --i) {
+            ans += i * a[i];
+        }
+        return ans % M;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<<ob.Maximize(a,n)<<endl;
+    }
+}
+// } Driver Code Ends
