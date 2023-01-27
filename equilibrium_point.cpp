e3a211b0bd683230aa08d7a513756a19cd397469
huggin <1171705+huggin@users.noreply.github.com>
2023-01-27 04:19:26 +0000

    Equilibrium Point

---
+++ equilibrium_point.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        // Your code here
+        int i = 0, j = n - 1;
+        long long left = 0, right = 0;
+        while(i < j) {
+            if (left <= right) left += a[i++];
+            else right += a[j--];
+        }
+        if (left == right) return i+1;
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
