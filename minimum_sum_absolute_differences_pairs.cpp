f5f4c95e8a7f79c39ea18482d74ed18c3d3ab0c8
huggin <1171705+huggin@users.noreply.github.com>
2023-01-28 21:42:24 +0000

    Minimum Sum of Absolute Differences of Pairs

---
+++ minimum_sum_absolute_differences_pairs.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    long long findMinSum(vector<int> &A,vector<int> &B,int N){
+        sort(A.begin(), A.end());
+        sort(B.begin(), B.end());
+        int ans = transform_reduce(
+            begin(A), end(A), begin(B), 0, 
+            std::plus<>{}, [](const int& a, const int& b) {return abs(a-b);}); 
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<int> A(N),B(N);
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+        for(int i=0;i<N;i++){
+            cin>>B[i];
+        }
+        Solution ob;
+        cout<<ob.findMinSum(A,B,N)<<endl;
+    }
+}
+// } Driver Code Ends
