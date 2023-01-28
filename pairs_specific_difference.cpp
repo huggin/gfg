917fa85f6daa5b8c11783b822b370abba4a537e3
huggin <rwangth@gmail.com>
2023-01-28 21:30:51 +0000

    Pairs with specific difference

---
+++ pairs_specific_difference.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int maxSumPairWithDifferenceLessThanK(int arr[], int N, int K)
+    {
+        // Your code goes here  
+        sort(arr, arr+N, greater<>{});
+        int ans = 0;
+        for(int i = 0; i < N;) {
+            if (i + 1 < N && arr[i] - arr[i+1] < K) {
+                ans += arr[i] + arr[i+1];
+                i+=2;
+            } else {
+                ++i;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int N;
+		cin>>N;
+		int arr[N];
+		for(int i=0;i<N;i++)
+		cin>>arr[i];
+
+		int K;
+		cin>>K;
+        Solution ob;
+		cout<<ob.maxSumPairWithDifferenceLessThanK(arr,N,K)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
