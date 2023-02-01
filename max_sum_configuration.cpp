27b746f6a4843cec26795592d23d077248506bf3
huggin <1171705+huggin@users.noreply.github.com>
2023-02-01 00:48:29 -0800

    Max sum in the configuration

---
+++ max_sum_configuration.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int max_sum(int A[],int N);
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>A[i];
+        }
+
+        cout<<max_sum(A,N)<<endl;
+        /*keeping track of the total sum of the array*/
+
+    }
+}
+
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+int max_sum(int A[],int N)
+{
+//Your code here
+    int t = 0, ans = INT_MIN, now = 0;
+    for(int i = 0; i < N; ++i) {
+        t += A[i];
+        now += i * A[i];
+    }
+    ans = max(now, ans);
+    for(int i = 0; i < N; ++i) {
+        now = (now - t) + A[i] * N;
+        ans = max(now, ans);
+    }
+    return ans;
+}
