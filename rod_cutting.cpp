a82aea2bac77674b60f9f6e267d15727df80ae17
huggin <rwangth@gmail.com>
2023-02-01 00:50:49 -0800

    Rod Cutting

---
+++ rod_cutting.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+  public:
+    int cutRod(int price[], int n) {
+        //code here
+        vector<int> dp(n);
+        dp[0] = price[0];
+        for(int i = 1; i < n; ++i) {
+            dp[i] = price[i];
+            for(int j = 0; j < i; ++j) {
+                dp[i] = max(dp[i], dp[j] + price[i-j-1]);
+            }
+        }
+        
+        return dp[n-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+            
+        Solution ob;
+
+        cout << ob.cutRod(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
