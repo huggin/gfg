d6e03dc28f4597e4aa1e2748d8a77a20733d5d5e
huggin <1171705+huggin@users.noreply.github.com>
2023-02-02 14:44:42 -0800

    Smallest sum contiguous subarray

---
+++ smallest_sum_contiguous_subarray.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+  int smallestSumSubarray(vector<int>& a){
+      //Code here
+      int ans = INT_MAX;
+      int total = 0;
+      for(int i = 0; i < a.size(); ++i) {
+          if (a[i] >= 0) {
+              if (total + a[i] < 0) {
+                  total += a[i];
+              } else {
+                  ans = min(ans, a[i]);
+                  total = 0;
+              }
+          } else {
+              total += a[i];
+              ans = min(ans, total);
+          }
+      }
+      return ans;
+  }
+};
+
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        
+        Solution ob;
+        
+        int ans = ob.smallestSumSubarray(a);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
