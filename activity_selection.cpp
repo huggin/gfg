a745d32e7d7a7b16d70cf1cba1418c65c2bc2fe4
huggin <1171705+huggin@users.noreply.github.com>
2023-02-03 05:20:21 -0800

    Activity Selection

---
+++ activity_selection.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of activities that can
+    //be performed by a single person.
+    int activitySelection(vector<int> start, vector<int> end, int n)
+    {
+        // Your code here
+        vector<pair<int, int>> v(n);
+        for(int i = 0; i < n; ++i) {
+            v[i] = make_pair(end[i], start[i]);
+        }
+        sort(v.begin(), v.end());
+        int ans = 1;
+        int pe = v[0].first;
+        for(int i = 1; i < n; ++i) {
+            if (v[i].second > pe) {
+                ++ans;
+                pe = v[i].first;
+            } 
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    while(t--)
+    {
+        //size of array
+        int n;
+        cin >> n;
+        vector<int> start(n), end(n);
+        
+        //adding elements to arrays start and end
+        for(int i=0;i<n;i++)
+            cin>>start[i];
+        for(int i=0;i<n;i++)
+            cin>>end[i];
+        Solution ob;
+        //function call
+        cout << ob.activitySelection(start, end, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
