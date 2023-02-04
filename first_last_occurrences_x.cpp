0b50b1151c65de6a783d1ce61b2e7a22c1cc6703
huggin <1171705+huggin@users.noreply.github.com>
2023-02-03 19:11:27 -0800

    First and last occurrences of X

---
+++ first_last_occurrences_x.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> firstAndLast(vector<int> &arr, int n, int x) {
+        // Code here
+        auto it = equal_range(arr.begin(), arr.end(), x);
+        if (it.first == arr.end() || *(it.first) != x) return vector<int>{-1};
+        return vector<int>{it.first-arr.begin(), it.second-arr.begin()-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        Solution obj;
+        vector<int> ans= obj.firstAndLast(arr, n, x) ;
+        for(int i:ans){
+            cout<<i<<" ";
+        }
+        cout<< endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
