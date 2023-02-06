f8170267919e10c7ade01dd859e849cbec9e1c93
huggin <rwangth@gmail.com>
2023-02-06 15:46:17 -0800

    Maximum no of 1's row

---
+++ maximum_no_1_row.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        int maxOnes (vector <vector <int>> &Mat, int N, int M)
+        {
+            // your code here
+            int pos = M;
+            int ans = INT_MAX;
+            for(int i = 0; i < N; ++i) {
+                int one = lower_bound(Mat[i].begin(), Mat[i].end(), 1) - Mat[i].begin();
+                if (one < pos) {
+                    pos = one;
+                    ans = i;
+                }
+            }
+            return ans;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t; cin >> t;
+    while (t--){
+        int n, m; cin >> n >> m;
+        vector <vector <int>> arr (n, vector <int> (m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> arr[i][j];
+        Solution ob;        
+        cout << ob.maxOnes(arr, n, m) << endl;
+    }
+}
+// } Driver Code Ends
