37c1a2e41ac4f1fa2b0895324299690e43dca736
huggin <rwangth@gmail.com>
2023-02-07 15:58:14 -0800

    Implement strstr

---
+++ implement_strstr.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+//Function to locate the occurrence of the string x in the string s.
+int strstr(string s, string x)
+{
+    //Your code here
+    for(int i = 0; i < s.size(); ++i) {
+        int k = i;
+        int j = 0;
+        for(; j < x.size(); ++j) {
+            if (s[k] == x[j]) {
+                ++k;
+            } else {
+                j = 0;
+                break;
+            }
+        }
+        if (j == x.size()) return i;
+    }
+    return -1;
+}
