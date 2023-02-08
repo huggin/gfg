c9b99ced984e80e63de7a5bd64c8ea7354c4a3f9
huggin <rwangth@gmail.com>
2023-02-07 21:03:49 -0800

    License Key Formatting

---
+++ license_key_formatting.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+class Solution
+{
+   public:
+    string ReFormatString(string S, int K){
+    	reverse(S.begin(), S.end());
+    	string ans;
+    	ans.reserve(S.size());
+    	int k = 0;
+    	for(auto c : S) {
+    	    if (c == '-') continue;
+    	    if (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z') {
+    	        ans.push_back(c);
+    	    } else {
+    	        ans.push_back(c - 'a' + 'A');
+    	    }
+    	    ++k;
+    	    if (k % K == 0) {
+    	        ans.push_back('-');
+    	    }
+    	}
+    	if (ans[ans.size()-1] == '-') {
+    	    ans.pop_back();
+    	}
+    	reverse(ans.begin(), ans.end());
+    	return ans;
+    	
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string S;
+	    cin>>S;
+	    int K;
+	    cin >> K;
+	    Solution ob;  
+	    string ans=ob.ReFormatString(S, K);
+	    cout<<ans;
+	    cout<<"\n";
+	}
+	return 0;
+}
+
+// } Driver Code Ends
