c729a473a6b257935189e519aaa8aa50c15fd2c1
huggin <1171705+huggin@users.noreply.github.com>
2023-02-07 16:15:16 -0800

    Minimum Swaps for Bracket Balancing

---
+++ minimum_swaps_bracket_balancing.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int minimumNumberOfSwaps(string S){
+        // code here 
+        int ans = 0;
+        int right = 0;
+        for(int i = 0; i < S.size(); ++i) {
+            if (S[i] == ']') {
+                ++right;
+            } else {
+                if (right > 0) {
+                    ans += right;
+                }
+                --right;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.minimumNumberOfSwaps(S) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
