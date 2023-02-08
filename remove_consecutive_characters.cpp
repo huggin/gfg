1fbc8297aa8aea080e5b4e6d70cc84dff30fdeb9
huggin <1171705+huggin@users.noreply.github.com>
2023-02-07 16:00:06 -0800

    Remove Consecutive Characters

---
+++ remove_consecutive_characters.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string S)
+    {
+        // code here.
+        auto it = unique(S.begin(), S.end());
+        S.erase(it, S.end());
+        return S;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+
+// } Driver Code Ends
