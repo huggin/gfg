0e49eb8476be183f64e3b42fa05400628c1d0ddd
huggin <1171705+huggin@users.noreply.github.com>
2023-02-07 16:11:02 -0800

    Reverse a string using Stack

---
+++ reverse_string_stack.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+char* reverse(char *str, int len);
+int main()
+{
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        char str[10000];
+        cin>>str;
+        long long int len=strlen(str);
+        char *ch=reverse(str,len);
+        for(int i=0;i<len;i++)
+        {
+            cout<<ch[i];
+        }
+        cout<<endl;
+    }
+        return 0;
+}
+
+
+// } Driver Code Ends
+
+
+//return the address of the string
+char* reverse(char *S, int len)
+{
+    //code here
+    stack<char> s;
+    for(int i = 0; i < len; ++i) {
+        s.push(S[i]);
+    }
+    char *ans = (char*)malloc(len);
+    int i = 0;
+    while(!s.empty()) {
+        ans[i] = s.top();
+        s.pop();
+        ++i;
+    }
+    return ans;
+}
