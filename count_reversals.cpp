ec259f9420bf999822ceae72ce3cd947ad75d7bb
huggin <1171705+huggin@users.noreply.github.com>
2023-02-07 20:45:55 -0800

    Count the Reversals

---
+++ count_reversals.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int countRev (string s);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        cout << countRev (s) << '\n';
+    }
+}
+
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+
+int countRev (string s)
+{
+    // your code here
+    if (s.size() % 2 == 1) return -1;
+    stack<char> S;
+    int ans = 0;
+    for(auto c : s) {
+        if (S.empty()) {
+            if (c == '}') {
+                ++ans;
+            }
+            S.push('{');
+        } else {
+            if (c == '{') {
+                S.push('{');
+            } else {
+                S.pop();
+            }
+        }
+    }
+    ans += S.size() / 2;
+    return ans;
+}
