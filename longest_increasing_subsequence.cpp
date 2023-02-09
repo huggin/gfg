129f15e08fc045c034fc89f61c61957dad09eb72
huggin <rwangth@gmail.com>
2023-02-09 14:59:27 -0800

    Longest Increasing Subsequence

---
+++ longest_increasing_subsequence.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[])
+    {
+       // your code here
+       vector<int> ans(n, 1);
+       for(int i=1; i<n; ++i) {
+           for(int j=0; j<i; ++j) {
+               if (a[i] > a[j]) {
+                   ans[i] = max(ans[i], ans[j]+1);
+               }
+           }
+       }
+
+       return *max_element(ans.begin(), ans.end()); 
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //taking size of array
+        cin>>n;
+        int a[n];
+        
+        //inserting elements to the array
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        //calling method longestSubsequence()
+        cout << ob.longestSubsequence(n, a) << endl;
+    }
+}
+
+// } Driver Code Ends
