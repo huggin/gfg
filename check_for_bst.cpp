644d3315605d0c015fb0852afb6261a0eeb19811
huggin <1171705+huggin@users.noreply.github.com>
2023-02-08 17:29:22 -0800

    Check for BST

---
+++ check_for_bst.cpp
@@ -0,0 +1,160 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    bool is(Node* root, int& mi, int& ma) {
+        if (root == NULL) {
+            return true;
+        }
+        mi = root->data;
+        ma = root->data;
+        if (root->left == NULL && root->right == NULL) {
+            return true;
+        }
+        if (root->left) {
+            int left_mi = 0, left_ma = 0;
+            bool is_left = is(root->left, left_mi, left_ma);
+            if (!is_left || left_ma >= root->data) return false;
+            mi = left_mi;
+        }
+        if (root->right) {
+            int right_mi = 0, right_ma = 0;
+            bool is_right = is(root->right, right_mi, right_ma);
+            if (!is_right || right_mi <= root->data) return false;
+            ma = right_ma;
+        }
+        return true;
+    }
+public:
+    //Function to check whether a Binary Tree is BST or not.
+    bool isBST(Node* root) 
+    {
+        // Your code here
+        if (root == NULL) return true;
+        int mi = INT_MAX;
+        int ma = INT_MIN;
+        return is(root, mi, ma);
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+void inorder(Node *root, vector<int> &v)
+{
+    if(root==NULL)
+        return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+ 
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+    string s; 
+    getline(cin, s);
+    Node* root = buildTree(s);
+    Solution ob;
+    if(ob.isBST(root)) 
+        cout<<"1\n";
+        
+    else
+        cout<<"0\n";
+   }
+   return 0;
+}
+
+
+// } Driver Code Ends
