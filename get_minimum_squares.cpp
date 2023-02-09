2b6b19d36e416e2075be22f44c527492dd2c1071
huggin <rwangth@gmail.com>
2023-02-09 15:14:10 -0800

    Get Minimum Squares

---
+++ get_minimum_squares.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+	public:
+	int MinSquares(int n)
+	{
+	    // Code here
+	    vector<int> v(n+1, INT_MAX);
+	    vector<int> t;
+	    for(int i=1; i*i<=10000; ++i) {
+	        t.push_back(i*i);
+	    }
+	    for(int i=1; i<=n; ++i) {
+	        if (find(t.begin(), t.end(), i) != t.end()) {
+	            v[i] = 1;
+	            continue;
+	        }
+	        for(int j=1; j*j<=i; ++j) {
+	            v[i] = min(v[i], v[i-j*j]+1);
+	        }
+	    }
+	    return v[n];
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans = ob.MinSquares(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
