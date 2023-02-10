30df634c5a247a6804b04927260e0f282ade79f1
huggin <1171705+huggin@users.noreply.github.com>
2023-02-09 20:55:35 -0800

    Maximum sum difference

---
+++ maximum_sum_difference.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int maxSum(int N){
+        return N * (N - 1) / 2 + (N / 2 - 1);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.maxSum(N)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
