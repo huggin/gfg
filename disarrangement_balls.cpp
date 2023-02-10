a34625485ec577fbe462cad4bca6f608067bf810
huggin <1171705+huggin@users.noreply.github.com>
2023-02-09 20:54:42 -0800

    Disarrangement of balls

---
+++ disarrangement_balls.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial function Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    long int disarrange(int N){
+        // code here
+        const int MOD = 1000000007;
+        long long dp[N+1];
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 2;
+        for(int i = 4; i <= N; ++i) {
+            dp[i] = (dp[i-1] + dp[i-2]) * (i-1) % MOD;
+        }
+        return dp[N];
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.disarrange(N)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code End
