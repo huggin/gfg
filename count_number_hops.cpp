ea51d0f336e6ad88f4afd1d7e6af435146bcf6f3
huggin <1171705+huggin@users.noreply.github.com>
2023-02-11 05:52:23 -0800

    Bug fix

--- count_number_hops.cpp
+++ count_number_hops.cpp
@@ -9,12 +9,13 @@ class Solution {
   // Function to count the number of ways in which frog can reach the top.
   long long countWays(int n) {
     // your code here
+    const int M = 1e9 + 7;
     vector<long long> dp(n + 1);
     dp[0] = 1;
     dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= n; ++i) {
-      dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % 1000000007;
+    for (int i = 2; i <= n; ++i) {
+      dp[i] = (dp[i - 2] + dp[i - 1]) % M;
+      if (i - 3 >= 0) dp[i] = (dp[i] + dp[i - 3]) % M;
     }
     return dp[n];
   }
