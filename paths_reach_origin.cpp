e207472c7f024da30aa5ab3da63cc8d8bed91ccb
huggin <1171705+huggin@users.noreply.github.com>
2023-02-12 15:40:52 -0800

    Paths to reach origin

---
+++ paths_reach_origin.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+ public:
+  int ways(int x, int y) {
+    // code here.
+    vector<vector<int>> dp(x + 1, vector<int>(y + 1, 0));
+    for (int i = 0; i <= x; ++i) {
+      dp[i][0] = 1;
+    }
+    for (int j = 0; j <= y; ++j) {
+      dp[0][j] = 1;
+    }
+    for (int i = 1; i <= x; ++i) {
+      for (int j = 1; j <= y; ++j) {
+        dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
+      }
+    }
+    return dp[x][y];
+  }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int x, y;
+    cin >> x >> y;
+    Solution ob;
+    cout << ob.ways(x, y) << endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
