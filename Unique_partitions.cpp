8eb1e52bd1b25c3e8cbe05b771b88bbc5f6e4b73
huggin <rwangth@gmail.com>
2023-02-12 16:35:01 -0800

    Unique partitions

---
+++ Unique_partitions.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  vector<vector<int>> ans;
+
+  void go(int k, int m, vector<int>& c) {
+    if (k == 0) {
+      ans.push_back(c);
+      return;
+    }
+    for (int i = m; i > 0; --i) {
+      c.push_back(i);
+      go(k - i, min(i, k - i), c);
+      c.pop_back();
+    }
+  }
+
+ public:
+  vector<vector<int>> UniquePartitions(int n) {
+    // Code here
+    ans.clear();
+    vector<int> c;
+    go(n, n, c);
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n;
+    cin >> n;
+    Solution ob;
+    vector<vector<int>> ans = ob.UniquePartitions(n);
+    for (auto i : ans)
+      for (auto j : i) cout << j << " ";
+    cout << "\n";
+  }
+  return 0;
+}
+// } Driver Code Ends
