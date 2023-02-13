c9b859596e80304245fc1a5c5bc8a61f22462712
huggin <1171705+huggin@users.noreply.github.com>
2023-02-12 17:12:10 -0800

    Maximum Sum Bitonic Subsequence

---
+++ maximum_sum_bitonic_subsequence.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSumBS(int arr[], int n);
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    cout << maxSumBS(a, n) << endl;
+  }
+}
+// } Driver Code Ends
+
+int maxSumBS(int arr[], int n) {
+  vector<int> dp(n);
+  for (int i = 0; i < n; ++i) {
+    dp[i] = arr[i];
+    for (int j = 0; j < i; ++j) {
+      if (arr[j] < arr[i]) {
+        dp[i] = max(dp[i], dp[j] + arr[i]);
+      }
+    }
+  }
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < i; ++j) {
+      if (arr[j] > arr[i]) {
+        dp[i] = max(dp[i], dp[j] + arr[i]);
+      }
+    }
+  }
+  return *max_element(dp.begin(), dp.end());
+}
