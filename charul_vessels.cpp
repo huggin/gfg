92a8d155907bef1f625f24fa7c916c507750c2dc
huggin <rwangth@gmail.com>
2023-02-13 15:24:32 -0800

    Charul and vessels

---
+++ charul_vessels.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool vessel(int a[], int n, int k);
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k;
+    cin >> n >> k;
+    int a[n + 1];
+
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    if (vessel(a, n, k)) {
+      cout << 1 << endl;
+    } else {
+      cout << 0 << endl;
+    }
+  }
+  return 0;
+}
+
+// } Driver Code Ends
+
+bool vessel(int a[], int n, int k) {
+  // Complete the function
+  vector<int> dp(k + 1);
+  dp[0] = 1;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j <= k; ++j) {
+      if (j - a[i] >= 0) dp[j] |= dp[j - a[i]];
+    }
+  }
+  return dp[k];
+}
