d08ed1050d5ca4e2842705a40ca44bbd173d2c2b
huggin <1171705+huggin@users.noreply.github.com>
2023-02-14 05:32:21 -0800

    Replace all 0's with 5

---
+++ replace_all_05.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver program to test above function
+
+// } Driver Code Ends
+class Solution {
+ public:
+  /*you are required to complete this method*/
+  int convertFive(int n) {
+    // Your code here
+    stringstream ss;
+    ss << n;
+    string s;
+    ss >> s;
+    replace(begin(s), end(s), '0', '5');
+    return atoi(s.c_str());
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int T;
+  cin >> T;
+  while (T--) {
+    int n;
+    cin >> n;
+    Solution obj;
+    cout << obj.convertFive(n) << endl;
+  }
+}
+// } Driver Code Ends
