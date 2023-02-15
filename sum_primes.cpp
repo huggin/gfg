61e152673fb58acc65910fc355cb4418febaaeae
huggin <1171705+huggin@users.noreply.github.com>
2023-02-14 17:16:03 -0800

    Sum of primes

---
+++ sum_primes.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+ public:
+  int primeSum(int N) {
+    // code here
+    int ans = 0;
+    while (N) {
+      int d = N % 10;
+      N /= 10;
+      if (d == 2 || d == 3 || d == 5 || d == 7) ans += d;
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int N;
+    cin >> N;
+
+    Solution ob;
+    cout << ob.primeSum(N) << "\n";
+  }
+  return 0;
+}
+// } Driver Code Ends
