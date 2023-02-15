f69bbb5642640a1bfd6a247c4268989eec9643a6
huggin <1171705+huggin@users.noreply.github.com>
2023-02-14 17:05:08 -0800

    Rectangles in N*N Board

---
+++ rectangles_board.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+ public:
+  long long rectanglesInChessBoard(long long N) {
+    // code here
+    return (N * (N + 1) / 2) * (N * (N + 1) / 2) -
+           N * (N + 1) * (2 * N + 1) / 6;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    long long N;
+
+    cin >> N;
+
+    Solution ob;
+    cout << ob.rectanglesInChessBoard(N) << endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
