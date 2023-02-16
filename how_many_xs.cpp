65c78648eedf141307a0b0bd2de016a6fe51b7a2
huggin <rwangth@gmail.com>
2023-02-15 22:08:24 -0800

    How Many X's?

---
+++ how_many_xs.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  int count(int n, int x) {
+    int ans = 0;
+    while (n > 0) {
+      if (n % 10 == x) ++ans;
+      n /= 10;
+    }
+    return ans;
+  }
+
+ public:
+  int countX(int L, int R, int X) {
+    // code here
+    int ans = 0;
+    for (int i = L + 1; i < R; ++i) {
+      ans += count(i, X);
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int L, R, X;
+    cin >> L >> R >> X;
+    Solution ob;
+    int ans = ob.countX(L, R, X);
+    cout << ans << "\n";
+  }
+}
+
+// } Driver Code Ends
