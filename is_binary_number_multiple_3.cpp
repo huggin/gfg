881f104ab5cb5da072b40ae386ee86596ecc71bc
huggin <1171705+huggin@users.noreply.github.com>
2023-02-20 07:23:41 -0800

    Is Binary Number Multiple of 3

---
+++ is_binary_number_multiple_3.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+ public:
+  int isDivisible(string s) {
+    // complete the function here
+    int ans = 0;
+    for (int i = s.size() - 1; i >= 0; --i) ans = (ans * 2 + s[i] - '0') % 3;
+    return ans == 0;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    Solution ob;
+    cout << ob.isDivisible(s) << endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
