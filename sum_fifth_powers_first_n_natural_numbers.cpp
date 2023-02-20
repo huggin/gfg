47371da76cf8ee8be34e9e2e50ea97a7e0bcfd85
huggin <rwangth@gmail.com>
2023-02-20 06:47:20 -0800

    Sum of fifth powers of the first n natural numbers

---
+++ sum_fifth_powers_first_n_natural_numbers.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+ public:
+  long long sumOfFifthPowers(long long N) {
+    // code here
+    long long ans = 0;
+    for (long long i = 1; i <= N; ++i) {
+      ans += i * i * i * i * i;
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int N;
+    cin >> N;
+    Solution ob;
+    cout << ob.sumOfFifthPowers(N) << "\n";
+  }
+}
+// } Driver Code Ends
