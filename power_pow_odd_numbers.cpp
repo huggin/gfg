26c11f10856c83c271a9f3630dfb40b8d66114cf
huggin <1171705+huggin@users.noreply.github.com>
2023-02-21 11:30:52 -0800

    Power of Pow | Odd Numbers

---
+++ power_pow_odd_numbers.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+ public:
+  long long int sum_of_square_oddNumbers(long long int n) {
+    // Code here.
+    long long ans = 0;
+    for (int i = 1; i <= n; ++i) {
+      ans += (2 * i - 1) * (2 * i - 1);
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int T;
+  cin >> T;
+  while (T--) {
+    long long int n;
+    cin >> n;
+    Solution ob;
+    long long int ans = ob.sum_of_square_oddNumbers(n);
+    cout << ans << "\n";
+  }
+  return 0;
+}
+// } Driver Code Ends
