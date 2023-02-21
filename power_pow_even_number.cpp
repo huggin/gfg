e4f3c96d192b94b176b398ad567527b519032cce
huggin <rwangth@gmail.com>
2023-02-21 10:08:38 -0800

    Power of Pow | Even Number

---
+++ power_pow_even_number.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+ public:
+  long long int sum_of_square_evenNumbers(long long int n) {
+    // Code here
+    long long ans = 0;
+    for (int i = 2; i <= 2 * n; i += 2) ans += i * i;
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int T;
+  cin >> T;
+  while (T--) {
+    long long int n;
+    cin >> n;
+    Solution ob;
+    long long int ans = ob.sum_of_square_evenNumbers(n);
+    cout << ans << "\n";
+  }
+  return 0;
+}
+// } Driver Code Ends
