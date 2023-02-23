4e2151a7621d64c55d47ed2eb4d6c014907aa16f
huggin <rwangth@gmail.com>
2023-02-22 16:18:44 -0800

    Repeated sum of digits

---
+++ repeated_sum_digits.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+ public:
+  int repeatedSumOfDigits(int N) {
+    // code here
+    if (N < 10) return N;
+    int n = 0;
+    while (N) {
+      n += N % 10;
+      N /= 10;
+    }
+    return repeatedSumOfDigits(n);
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int N;
+    cin >> N;
+    Solution ob;
+    cout << ob.repeatedSumOfDigits(N) << endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
