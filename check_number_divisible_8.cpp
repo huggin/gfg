7b6b2f309c4b77b8f8f12408f0a23e49984a6781
huggin <1171705+huggin@users.noreply.github.com>
2023-02-22 20:43:25 -0800

    Check if a number is divisible by 8

---
+++ check_number_divisible_8.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+ public:
+  int DivisibleByEight(string S) {
+    // code here
+    int n = 0;
+    int m = S.size();
+    for (int i = max(0, m - 3); i < m; ++i) {
+      n = n * 10 + S[i] - '0';
+    }
+    return n % 8 == 0 ? 1 : -1;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    string S;
+    cin >> S;
+    Solution ob;
+    cout << ob.DivisibleByEight(S) << "\n";
+  }
+}
+// } Driver Code Ends
