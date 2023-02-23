faeac51eaea18c94a411279e35804a39171696c7
huggin <1171705+huggin@users.noreply.github.com>
2023-02-23 12:54:40 -0800

    Check Binary String

---
+++ check_binary_string.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+ public:
+  bool checkBinary(string s) {
+    // Your Code Here
+    int i = 0;
+    for (char c : s) {
+      if (c == '1') {
+        if (i == 2) return false;
+        i = 1;
+      } else {
+        if (i == 1) i = 2;
+      }
+    }
+    return true;
+  }
+};
+
+//{ Driver Code Starts.
+
+int32_t main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    Solution ob;
+    if (ob.checkBinary(s))
+      cout << "VALID\n";
+    else
+      cout << "INVALID\n";
+  }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
