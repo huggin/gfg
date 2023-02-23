87c3cc4d22dafb459ba2c6eb963a6cfa8ee42a2a
huggin <1171705+huggin@users.noreply.github.com>
2023-02-22 20:46:23 -0800

    Check if actual binary representation of a number is palindrome

---
+++ check_binary_representation_number_palindrome.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+ public:
+  int binaryPalin(long long int N) {
+    // Your Code Here
+    vector<int> v;
+    while (N) {
+      v.push_back(N % 2);
+      N /= 2;
+    }
+    for (int i = 0; i < v.size() / 2; ++i)
+      if (v[i] != v[v.size() - 1 - i]) return false;
+
+    return true;
+  }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    long long int n;
+    cin >> n;
+    Solution ob;
+    cout << ob.binaryPalin(n) << endl;
+  }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
