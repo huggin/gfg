ed20361d40d0917b1022db79b01a58d837036f37
huggin <1171705+huggin@users.noreply.github.com>
2023-02-22 22:29:43 -0800

    Kth Prime Factor

---
+++ kth_prime_factor.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+ public:
+  int kthPrime(int n, int k) {
+    // code here
+    for (int i = 2; i <= n; ++i) {
+      while (n % i == 0) {
+        if (--k == 0) return i;
+        n /= i;
+      }
+    }
+    return -1;
+  }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k;
+    cin >> n >> k;
+
+    Solution ob;
+    cout << ob.kthPrime(n, k) << "\n";
+  }
+  return 0;
+}
+// } Driver Code Ends
