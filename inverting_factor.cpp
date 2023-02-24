f01342bde7449d87603916b0c6950fe98f111d18
huggin <rwangth@gmail.com>
2023-02-23 23:49:05 -0800

    The Inverting Factor

---
+++ inverting_factor.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find the minimum inverting factor
+int findMinimumInvertingFactor(int arr[], int N);
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << findMinimumInvertingFactor(arr, n);
+
+    cout << endl;
+  }
+}
+
+// } Driver Code Ends
+
+int findMinimumInvertingFactor(int arr[], int N) {
+  // Complete the function
+  transform(arr, arr + N, arr, [](int a) {
+    int ans = 0;
+    while (a) {
+      ans = ans * 10 + a % 10;
+      a /= 10;
+    }
+    return ans;
+  });
+  sort(arr, arr + N);
+  int ans = INT_MAX;
+  for (int i = 1; i < N; ++i) {
+    ans = min(ans, arr[i] - arr[i - 1]);
+  }
+  return ans;
+}
