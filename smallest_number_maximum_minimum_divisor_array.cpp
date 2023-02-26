88104f2bee7840ef65802cebe6ca2e662c8dbbda
huggin <rwangth@gmail.com>
2023-02-26 01:09:55 -0800

    Smallest number with maximum and minimum divisor in an array

---
+++ smallest_number_maximum_minimum_divisor_array.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+ public:
+  int getNum(int arr[], int N) {
+    // code here
+    auto mm = minmax_element(arr, arr + N);
+    int a = *mm.first;
+    int b = *mm.second;
+
+    return a * b / __gcd(a, b);
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int N;
+
+    cin >> N;
+    int arr[N];
+
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    Solution ob;
+    cout << ob.getNum(arr, N) << endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
