d58f0b991803b0af84d95730b6f74d8a1c734d2a
huggin <rwangth@gmail.com>
2023-02-26 12:28:08 -0800

    Mathematical manipulation

---
+++ mathematical_manipulation.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+ public:
+  int CommonFactor(int n) {
+    // Code here
+    int ans = 0;
+    for (int i = 2; i <= n; ++i) {
+      if (__gcd(i, n) != 1) ++ans;
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n;
+    cin >> n;
+    Solution obj;
+    int ans = obj.CommonFactor(n);
+    cout << ans << "\n";
+  }
+  return 0;
+}
+// } Driver Code Ends
