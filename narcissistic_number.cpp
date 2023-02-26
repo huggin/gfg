030b40df1b2b2f270cea66a2ed0fcc1811000b8d
huggin <1171705+huggin@users.noreply.github.com>
2023-02-26 12:19:52 -0800

    Narcissistic number

---
+++ narcissistic_number.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+ public:
+  int isNarcissistic(int N) {
+    // code here
+    int n = 0;
+    int t = N;
+    while (t > 0) {
+      ++n;
+      t /= 10;
+    }
+    t = N;
+    int ans = 0;
+    while (t > 0) {
+      int d = t % 10;
+      ans += pow(d, n);
+      t /= 10;
+    }
+    return ans == N;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int N;
+    cin >> N;
+    Solution ob;
+    cout << ob.isNarcissistic(N) << endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
