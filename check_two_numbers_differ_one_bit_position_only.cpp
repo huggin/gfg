ad03e28ed66037c3167aa4152b67262d0741a6d7
huggin <rwangth@gmail.com>
2023-02-27 20:56:27 -0800

    Check whether two numbers differ at one bit position only

---
+++ check_two_numbers_differ_one_bit_position_only.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+ public:
+  int DifferOne(long int a, long int b) {
+    // complete the function here
+    int n = a ^ b;
+    return !(n & (n - 1));
+  }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    long int a, b;
+    cin >> a >> b;
+    Solution ob;
+    cout << ob.DifferOne(a, b) << endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
