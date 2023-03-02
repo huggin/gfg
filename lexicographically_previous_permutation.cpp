4a0749e3144dedd575b773858ec9d91543f4e381
huggin <rwangth@gmail.com>
2023-03-02 14:32:49 -0800

    Lexicographically Previous Permutation

---
+++ lexicographically_previous_permutation.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+ public:
+  string prevPermutation(string s) {
+    // code here
+    if (prev_permutation(s.begin(), s.end())) return s;
+    return "None";
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    Solution ob;
+    cout << ob.prevPermutation(s) << endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
