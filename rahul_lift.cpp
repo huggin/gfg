e0e0362da41ceebb25501252c5bf7fb54dabae95
huggin <1171705+huggin@users.noreply.github.com>
2023-03-02 18:38:49 -0800

    ThrowBall at Fair

---
+++ rahul_lift.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+ public:
+  int noOfWays(int z, int n, int m) {
+    // Code here
+    const int M = 1e9 + 7;
+    long long ans = 1;
+    long long k = z - 1;
+    for (int i = 1; i <= m; ++i) {
+      ans = (ans * k) % M;
+      ++k;
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n, z, m;
+    cin >> z >> n >> m;
+    Solution obj;
+    int ans = obj.noOfWays(z, n, m);
+    cout << ans << "\n";
+  }
+  return 0;
+}
+// } Driver Code Ends
---
+++ throwball_fair.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  vector<long long> fact;
+
+ public:
+  long long int count(long long int N, long long int K, long long int M) {
+    // code here
+    long long n = N - 1;
+    long long k = K - 1;
+    k = min(k, n - k);
+    long long ans = 1;
+    fact.assign(M + 1, 1);
+    for (int i = 1; i <= M; ++i) fact[i] = fact[i - 1] * i % M;
+
+    while (n) {
+      if (n % M < k % M) return 0;
+      ans = ans * fact[n % M] * inv(fact[k % M] * fact[(n - k) % M], M) % M;
+      n /= M;
+      k /= M;
+    }
+    return ans;
+  }
+
+  long long inv(long long d, int m) {
+    long long ans = 1;
+    int b = m - 2;
+    while (b) {
+      if (b & 1) ans = ans * d % m;
+      d = (d % m) * (d % m) % m;
+      b >>= 1;
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    long long int N;
+    cin >> N;
+    long long int K;
+    cin >> K;
+    long long int M;
+    cin >> M;
+    Solution ob;
+    cout << ob.count(N, K, M) << endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
