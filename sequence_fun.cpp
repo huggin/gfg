3bf5a04271d995f3a41a2f4eb15005d3d1e38aad
huggin <1171705+huggin@users.noreply.github.com>
2023-03-06 22:05:49 -0800

    Sequence Fun

---
+++ sequence_fun.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+ public:
+  int NthTerm(int n) {
+    // Code  here
+    const int M = 1e9 + 7;
+    long long ans = 2;
+    for (int i = 2; i <= n; ++i) {
+      ans = (ans * i + 1) % M;
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n;
+    cin >> n;
+    Solution obj;
+    int ans = obj.NthTerm(n);
+    cout << ans << "\n";
+  }
+  return 0;
+}
+// } Driver Code Ends
