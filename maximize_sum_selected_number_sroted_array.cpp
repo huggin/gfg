03b5b71cc4f464df32ebc839575aa7d75f681bcb
huggin <rwangth@gmail.com>
2023-03-08 22:44:19 -0800

    Maximize the sum of selected numbers from a sorted array to make it empty

---
+++ maximize_sum_selected_number_sroted_array.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+ public:
+  int maximizeSum(int a[], int n) {
+    // Complete the function
+    multiset<int> s(a, a + n);
+    int ans = 0;
+    while (!s.empty()) {
+      auto it = s.rbegin();
+      ans += *it;
+      int d = *it - 1;
+      ++it;
+      s.erase(it.base());
+      auto it2 = s.find(d);
+      if (it2 != s.end())
+        s.erase(it2);
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    Solution ob;
+
+    cout << ob.maximizeSum(arr, n) << endl;
+  }
+}
+
+// } Driver Code Ends
