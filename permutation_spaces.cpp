16ec18ba9a0a98abecb9bd15672135d1ab502ce9
huggin <rwangth@gmail.com>
2023-03-12 00:26:40 -0800

    Permutation with Spaces

---
+++ permutation_spaces.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  vector<string> ans;
+  string s;
+
+  void solve(int k, string& t) {
+    if (k == s.size()) {
+      ans.push_back(t);
+      return;
+    }
+    if (t.back() != ' ') {
+      t.push_back(' ');
+      solve(k, t);
+      t.pop_back();
+    }
+    t.push_back(s[k]);
+    solve(k + 1, t);
+    t.pop_back();
+  }
+
+ public:
+  vector<string> permutation(string S) {
+    // Code Here
+    s = S;
+    ans.clear();
+    string t(1, S[0]);
+    solve(1, t);
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    string S;
+    cin >> S;
+    vector<string> ans;
+    Solution obj;
+    ans = obj.permutation(S);
+    for (int i = 0; i < ans.size(); i++) {
+      cout << "(" << ans[i] << ")";
+    }
+    cout << endl;
+  }
+}
+
+// } Driver Code Ends
