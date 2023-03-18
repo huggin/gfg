e21ad6b600238ee16a2629f3dd57361764d5ec57
huggin <rwangth@gmail.com>
2023-03-17 23:12:58 -0700

    more tag

--- game/critical_connections.cpp
+++ game/critical_connections.cpp
@@ -1,3 +1,10 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
 class Solution {
   vector<int> low, dist, onStack, marked, to;
 
@@ -27,7 +34,7 @@ class Solution {
     }
   }
 
- public:
+public:
   vector<vector<int>> criticalConnections(int V, vector<int> adj[]) {
     // Code here
     marked.assign(V, 0);
@@ -46,3 +53,28 @@ class Solution {
     return ans;
   }
 };
+
+//{ Driver Code Starts.
+
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int V, E;
+    cin >> V >> E;
+    vector<int> adj[V];
+    for (int i = 0; i < E; i++) {
+      int u, v;
+      cin >> u >> v;
+      adj[u].push_back(v);
+      adj[v].push_back(u);
+    }
+    Solution obj;
+    vector<vector<int>> ans = obj.criticalConnections(V, adj);
+
+    for (int i = 0; i < ans.size(); i++)
+      cout << ans[i][0] << " " << ans[i][1] << endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
--- stl/cyclically_rotate.cpp
+++ stl/cyclically_rotate.cpp
@@ -12,9 +12,11 @@ int main() {
     int n;
     scanf("%d", &n);
     int a[n], i;
-    for (i = 0; i < n; i++) scanf("%d", &a[i]);
+    for (i = 0; i < n; i++)
+      scanf("%d", &a[i]);
     rotate(a, n);
-    for (i = 0; i < n; i++) printf("%d ", a[i]);
+    for (i = 0; i < n; i++)
+      printf("%d ", a[i]);
     printf("\n");
   }
   return 0;
