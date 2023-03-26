dbdf31f402ca58be8c53570a771b8a46f3784986
huggin <1171705+huggin@users.noreply.github.com>
2023-03-26 15:40:50 -0700

    Smallest power of 2 greater than or equal to n

---
+++ bit/smallest_power_2_greater_than_equal_n.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def nearestPowerOf2(self,N):
+        #code here
+        p = 1;
+        while N > p:
+            p *= 2
+        return p
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        N=int(input())
+        ob=Solution()
+        print(ob.nearestPowerOf2(N))
+# } Driver Code Ends
