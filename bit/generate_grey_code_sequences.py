d827a23a235e3d543906f3e6446792e356d587f2
huggin <1171705+huggin@users.noreply.github.com>
2023-03-27 20:55:21 -0700

    Generate Grey Code Sequences

---
+++ bit/generate_grey_code_sequences.py
@@ -0,0 +1,25 @@
+# Your task is to complete this function
+# Function should return a list of all the Grey Code Generated
+class Solution:
+    def generateCode(self, n):
+        # Code here
+        return ['{:0{n}b}'.format(x ^ (x >> 1), n=n) for x in range(1<<n)]
+
+#{ 
+ # Driver Code Starts
+
+
+# Driver Program
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        res = Solution().generateCode(int(input()))
+        for j in range (len (res)):
+            print (res[j], end = " ") 
+        print ()
+
+
+# Contributed By: Harshit Sidhwa
+
+# } Driver Code Ends
