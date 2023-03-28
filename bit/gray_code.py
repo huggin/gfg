127cf04c9926aa74c2e400cd0f32777698a49e0d
huggin <1171705+huggin@users.noreply.github.com>
2023-03-27 20:52:07 -0700

    Gray Code

---
+++ bit/gray_code.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+class Solution:
+    def graycode(self,n):
+        g = [x ^ (x >> 1) for x in range(1<<n)]
+        return ['{:0{n}b}'.format(x, n=n) for x in g]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import math
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        l=ob.graycode(n)
+        
+        for x in l:
+            print(x, end=" ")
+            
+        print()
+     
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
