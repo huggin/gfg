2cbb47d3668a14d06f91b32f375387b125a1e9ae
huggin <1171705+huggin@users.noreply.github.com>
2023-03-29 11:42:12 -0700

    Numbers having alternate bits in range 1 to n

---
+++ bit/numbers_having_alternate_bits.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+class Solution:
+    def count (self, N):
+        # code here
+        ans = []
+        n = 1
+        m = 0
+        while n <= N:
+            ans.append(n)
+            n = (n << 1) + m
+            m = 1 - m
+        return ans
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__': 
+    ob = Solution()
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+        ans = ob.count(N);
+        print(*ans)
+
+
+
+
+# } Driver Code Ends
