a1c337274b4ee5e881fc943ac098e129e443c643
huggin <rwangth@gmail.com>
2023-03-28 17:29:41 -0700

    XOR Count Zero and One

---
+++ bit/xor_count_zero_one.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+	def find_xor(self, n):
+		# Code here
+        s = bin(n)
+        one = 0
+        zero = 0
+        for i in range(2, len(s)):
+            if s[i] == '0':
+                zero += 1
+            else:
+                one += 1
+        
+        return one ^ zero
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		ob = Solution();
+		ans = ob.find_xor(n)
+		print(ans)
+
+# } Driver Code Ends
