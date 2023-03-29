31bae3d7574ace7764bc55acb0ac4a39e44ee3c0
huggin <1171705+huggin@users.noreply.github.com>
2023-03-29 11:23:03 -0700

    Binary representation of previous number

---
+++ bit/binary_representation_previous_number.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+class Solution:
+	def binaryPreviousNumber(self, S):
+		# code here
+        return bin(int(S, 2) - 1)[2:]
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		S = input()
+		ob = Solution()
+		ans = ob.binaryPreviousNumber(S)
+		print(ans)
+
+
+
+
+# } Driver Code Ends
