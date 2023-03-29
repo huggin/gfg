06312e7efaf1586cf6b7b19b60346ffaa2dce25d
huggin <1171705+huggin@users.noreply.github.com>
2023-03-29 13:02:31 -0700

    Maximize XOR

---
+++ bit/maximize_xor.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+	def maximize_xor_count(self, n):
+		# Code here
+        ans = 0
+        k = 1
+        while n:
+            if (n & 1) == 0:
+                ans += k
+            n >>= 1
+            k <<= 1
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		ob = Solution();
+		ans = ob.maximize_xor_count(n)
+		print(ans)
+
+
+# } Driver Code Ends
+
+
