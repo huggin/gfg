875a7c1da5b8e9e3d4fee37063a20d71c66dcca8
huggin <rwangth@gmail.com>
2023-03-29 11:38:52 -0700

    Maximize OR

---
+++ bit/maximize_or.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+class Solution:
+    def getCount (ob, X):
+        # code here 
+        cnt = 0
+        for i in range(31):
+            if X >= (1 << i):
+                if X & 1 << i:
+                    cnt += 1
+        
+        return X - 2 ** cnt + 1
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        X = input()
+        X = int(X)
+        
+        ob = Solution()
+        print(ob.getCount(X))
+# } Driver Code Ends
