168b796e99ef350bb075139cf3a837f2a603e0be
huggin <1171705+huggin@users.noreply.github.com>
2023-04-02 15:01:22 -0700

    Nth Natural Number

---
+++ math/nth_natural_number.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def findNth(self,N):
+        #code here
+        a = []
+        while N:
+            a.append(N % 9)
+            N //= 9
+        
+        ans = 0
+        for i in range(len(a)-1, -1, -1):
+            ans = ans * 10 + a[i]
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t=int(input())
+for i in range(0,t):
+    n=int(input())
+    obj=Solution()
+    s=obj.findNth(n)
+    print(s)
+# } Driver Code Ends
