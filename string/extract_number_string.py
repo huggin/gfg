6fa5434e015e1c4cb8f54b59202c2e9bf69df767
huggin <1171705+huggin@users.noreply.github.com>
2023-04-02 00:17:54 -0700

    Extract the Number from the String

---
+++ string/extract_number_string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def ExtractNumber(self,S):
+        #code here
+        ans = -1
+        for s in S.split():
+            if s.isdigit() and s.find('9') == -1:
+                 ans = max(ans, int(s))
+        
+        return ans
+
+#{ 
+ # Driver Code Starts
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        S=input()
+        ob=Solution()
+        ans=ob.ExtractNumber(S)
+        print(ans)   
+# } Driver Code Ends
