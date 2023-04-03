5aca1978a740eb3231fca8d54640b62e1800ba88
huggin <rwangth@gmail.com>
2023-04-03 11:45:58 -0700

    Minimum Steps Required

---
+++ greedy/minimum_steps_required.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minSteps(self, str : str) -> int:
+        # code here
+        cnt = {'a' : 0, 'b' : 0}
+        prev = 'c'
+        for s in str:
+            if s != prev:
+                cnt[s] += 1
+                prev = s
+        
+        return min(cnt['a'], cnt['b']) + 1
+        
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        str = (input())
+        
+        obj = Solution()
+        res = obj.minSteps(str)
+        
+        print(res)
+        
+
+# } Driver Code Ends
