510923c9c4f53a2b7fa8b9bfdcd6cc826eded4a8
huggin <1171705+huggin@users.noreply.github.com>
2023-04-03 23:41:40 -0700

    Rohan's Love for Matrix

---
+++ dp/rohans_love_matrix.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+class Solution:
+    def firstElement (self, n):
+        # code here 
+        m = 1000000007
+        a = 0
+        b = 1
+        for _ in range(n):
+            a, b = b, (a+b) % m
+        
+        return a
+        
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n = int(input())
+        
+        ob = Solution()
+        print(ob.firstElement(n))
+# } Driver Code Ends
