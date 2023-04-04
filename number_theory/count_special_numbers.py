d96e247c332fa86cb12922ca9f0a935963847d18
huggin <1171705+huggin@users.noreply.github.com>
2023-04-04 11:40:56 -0700

    Count Special Numbers

---
+++ number_theory/count_special_numbers.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:
+    def countSpecialNumbers(self, N, arr):
+        # Code here
+        ma = max(arr)
+        mark = [-1] * (ma + 1)
+        for a in arr:
+            if mark[a] == -1:
+                mark[a] = 1
+                for i in range(a+a, ma+1, a):
+                    mark[i] = 0
+            elif mark[a] == 1:
+                mark[a] = 0
+        
+        ans = 0
+        for a in arr:
+            if mark[a] == 0:
+                ans += 1
+        
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    
+    T = int(input())
+    while T > 0: 
+        N=int(input())
+        arr = [int(i) for i in input().split()]
+        ob = Solution()
+        print(ob.countSpecialNumbers(N, arr))
+        
+        T -= 1
+# } Driver Code Ends
