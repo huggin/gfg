80cd23e983b844730798e57c6ed0d4c5e4c96351
huggin <1171705+huggin@users.noreply.github.com>
2023-04-05 11:39:59 -0700

    Equal Left and Right Subarray Sum

---
+++ array/equal_left_right_subarray_sum.py
@@ -0,0 +1,45 @@
+# User function Template for python3
+
+class Solution:
+    # Complete this function
+    
+    #Function to find equilibrium point in the array.
+    def equalSum(self,A, N):
+        # Your code here
+        s = sum(A)
+        pre = 0
+        for i in range(N):
+            if pre == s - A[i] - pre:
+                return i+1
+            pre += A[i]
+        
+        return -1
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+import math
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+
+        N = int(input())
+
+        A = [int(x) for x in input().strip().split()]
+        ob=Solution()
+
+        print(ob.equalSum(A, N))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
