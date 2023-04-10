b8f866eaff9766a3f2429419ffe3b62fdc3dc0c5
huggin <1171705+huggin@users.noreply.github.com>
2023-04-10 11:39:48 -0700

    Maximum Length

---
+++ math/maximum_length.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution():
+    def solve(self, a, b, c):
+        #your code goes here
+        if a >= (b+c) * 2 + 3 or b >= (a+c) * 2 + 3 or c >= (a+b)*2 + 3:
+            return -1
+        return a+b+c
+        
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import Counter
+for _ in range(int(input())):
+    a, b, c=map(int,input().split())
+    obj = Solution()
+    res = obj.solve(a, b, c)
+    
+    print(res)
+# } Driver Code Ends
