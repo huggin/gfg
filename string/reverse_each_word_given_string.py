b85f25d2ae32d1aaa116ca630310ea260d34f355
huggin <1171705+huggin@users.noreply.github.com>
2023-04-10 11:10:08 -0700

    Reverse each word in a given string

---
+++ string/reverse_each_word_given_string.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+class Solution:
+    def reverseWords(self, s):
+        # code here
+        words = s.split('.')
+        return '.'.join(w[::-1] for w in words)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        ob = Solution()
+        ans = ob.reverseWords(s)
+        print(ans)
+# } Driver Code Ends
