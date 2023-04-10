3ac13e01532199c0816eabf41d43e35daed2108a
huggin <rwangth@gmail.com>
2023-04-10 11:05:40 -0700

    Column name from a given column number

---
+++ misc/column_name_given_column_number.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def colName (self, n):
+        # your code here
+        ans = []
+        while n:
+            n -= 1
+            ans.append(chr(n % 26 + ord('A')))
+            n //= 26
+        
+        return ''.join(reversed(ans))
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t = int (input ())
+for tc in range (t):
+    n = int (input ())
+    ob = Solution()
+    print (ob.colName (n))
+    
+
+# } Driver Code Ends
