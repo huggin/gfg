8b561b2bdaeeae6395dfcc14151473e2aa67d6ef
huggin <rwangth@gmail.com>
2023-04-10 17:12:05 -0700

    Rotate and delete

---
+++ math/rotate_delete.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+public:
+
+  // function to do operations of rotate and delete
+  // v: input vector
+  // n: size of vector
+  int rotateDelete(vector<long long> v, int n){
+
+    // Your code here
+    if (n == 1) return v[0];
+    if (n & 1) {
+      return v[(n-3)/4 + 2];
+    }
+    return v[(n-2)/4+1];
+  }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+  int i, n, t,te;
+  cin>>te;
+
+  while(te--)
+  {
+    cin>>n;
+    vector <long long> v;
+    for(i=0; i<n; i++)
+    {
+      cin>>t;
+      v.push_back(t);
+    }
+    Solution ob;
+    cout << ob.rotateDelete(v, n) << endl;
+  }
+
+  return 0;
+}
+// } Driver Code Ends
