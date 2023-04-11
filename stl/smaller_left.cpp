3a4b82f3a661394d1b8f9228499ee43795d9aeb4
huggin <rwangth@gmail.com>
2023-04-10 18:09:38 -0700

    Smaller on Left

---
+++ stl/smaller_left.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> Smallestonleft(int arr[], int n);
+
+int main() {
+
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    int arr[n+1];
+
+    for(int i=0;i<n;i++){
+      cin>> arr[i];
+    }
+
+    vector<int> b;
+    b = Smallestonleft(arr, n);
+    for(int i=0;i<n;i++)
+      cout << b[i] << " ";
+
+    cout << endl;
+  }
+
+  return 0;
+}
+// } Driver Code Ends
+
+
+vector<int> Smallestonleft(int arr[], int n)
+{
+  // Complete the function
+  set<int> s;
+  vector<int> ans(n);
+  for(int i = 0; i < n; ++i) {
+    if (s.empty()) {
+      ans[i] = -1;
+    } else {
+      auto it = s.lower_bound(arr[i]);
+      --it;
+      if (it != s.end() && *it < arr[i])
+        ans[i] = *it;
+      else
+        ans[i] = -1;
+    }
+    s.insert(arr[i]);
+  }
+  return ans;
+}
