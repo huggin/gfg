49c72e46b3bf76fa3e47f71d4cec0ce0608e930e
huggin <rwangth@gmail.com>
2023-04-15 13:27:53 -0700

    Reverse alternate nodes in Link List

---
+++ linked_list/reverse_alternate_nodes_link_list.py
@@ -0,0 +1,76 @@
+"""
+  reverse alternate nodes and append at the end
+  The input list will have at least one element
+  Node is defined as
+class Node:
+
+	# Constructor to initialize the node object
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+"""
+class Solution:
+    def rearrange(self, head):
+        # Code here
+        prev = None
+        p = head
+        while p:
+            n = p.next
+            if not n:
+                break
+            p.next = n.next
+            n.next = prev
+            prev = n
+            if not p.next:
+                break
+            p = p.next
+            
+        p.next = prev
+        return head
+
+#{ 
+ # Driver Code Starts
+# Node Class
+class Node:
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+# Linked list class contains node object
+class LinkedList:
+    # Constructor to initialize head
+    def __init__(self):
+        self.head = None
+
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+
+# Code execution starts here
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in reversed(values):
+            llist.push(i)
+            
+        Solution().rearrange(llist.head)
+        llist.printList()
+        t -= 1
+# Contributed by: Harshit Sidhwa
+# } Driver Code Ends
