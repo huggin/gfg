c3440c44275d6a972f1f5f63015ba4d151e54bcf
huggin <rwangth@gmail.com>
2023-04-21 11:51:09 -0700

    Smaller Sum

--- array/equal_left_right_subarray_sum.py
+++ array/equal_left_right_subarray_sum.py
@@ -1,10 +1,13 @@
 # User function Template for python3
 
+import math
+
+
 class Solution:
     # Complete this function
-    
-    #Function to find equilibrium point in the array.
-    def equalSum(self,A, N):
+
+    # Function to find equilibrium point in the array.
+    def equalSum(self, A, N):
         # Your code here
         s = sum(A)
         pre = 0
@@ -12,27 +15,25 @@ class Solution:
             if pre == s - A[i] - pre:
                 return i+1
             pre += A[i]
-        
+
         return -1
 
 
-#{ 
+# {
  # Driver Code Starts
 # Initial Template for Python 3
-
-import math
 
 
 def main():
 
     T = int(input())
 
-    while(T > 0):
+    while (T > 0):
 
         N = int(input())
 
         A = [int(x) for x in input().strip().split()]
-        ob=Solution()
+        ob = Solution()
 
         print(ob.equalSum(A, N))
 
---
+++ binary_search/smaller_sum.py
@@ -0,0 +1,58 @@
+from typing import List
+import bisect
+
+
+class Solution:
+    def smallerSum(self, n: int, arr: List[int]) -> List[int]:
+        # code here
+        d = []
+        for i in range(n):
+            d.append((arr[i], i))
+
+        d.sort()
+        prefix = []
+        total = 0
+        for i in range(n):
+            prefix.append(total)
+            total += d[i][0]
+
+        ans = [0] * n
+        for i in range(n):
+            idx = bisect.bisect_left(d, d[i][0], key = lambda x : x[0])
+            # idx = bisect.bisect_left(d, (d[i][0], 0))
+            ans[d[i][1]] = prefix[idx]
+
+        return ans
+
+
+# {
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  # array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        arr = IntArray().Input(n)
+
+        obj = Solution()
+        res = obj.smallerSum(n, arr)
+
+        IntArray().Print(res)
+
+
+# } Driver Code Ends
