c23a5cceb659a1f4cfbfb93d4715144a3deca5d6
huggin <rwangth@gmail.com>
2023-04-22 15:16:01 -0700

    Max value after m range operation

---
+++ array/max_value_m_range_operation.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findMax(self, n, m, a, b, c):
+        # Your code goes here
+        ans = [0] * (n + 1)
+        for i in range(m):
+            ans[a[i]] += c[i]
+            ans[b[i]+1] -= c[i]
+
+        for i in range(1, n+1):
+            ans[i] += ans[i-1]
+
+        return max(ans)
+
+
+# {
+ # Driver Code Starts
+if __name__ == '__main__':
+
+    t = int(input())
+    for _ in range(0, t):
+        l = list(map(int, input().split()))
+        n = l[0]
+        m = l[1]
+        a = []
+        b = []
+        c = []
+        for j in range(0, m):
+            v = list(map(int, input().split()))
+            a.append(v[0])
+            b.append(v[1])
+            c.append(v[2])
+        ob = Solution()
+        print(ob.findMax(n, m, a, b, c))
+# } Driver Code Ends
