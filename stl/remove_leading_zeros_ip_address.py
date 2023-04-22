eb5f60b591fb91db1ca642e6d29935eaf0a817ff
huggin <rwangth@gmail.com>
2023-04-22 14:11:04 -0700

    Remove leading zeros from an IP address

---
+++ stl/remove_leading_zeros_ip_address.py
@@ -0,0 +1,21 @@
+# User function Template for python3
+
+class Solution:
+    def newIPAdd(self, S):
+        # code here
+        ip = list(map(int, S.split('.')))
+        return '.'.join(map(str, ip))
+
+# {
+ # Driver Code Starts
+# Initial Template for Python 3
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        ob = Solution()
+        ans = ob.newIPAdd(s)
+        print(ans)
+# } Driver Code Ends
