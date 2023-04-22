acc17b29fbd1bbc8fccda12750881cbe24cc2c1f
huggin <rwangth@gmail.com>
2023-04-22 12:10:19 -0700

    Minimum Number

---
+++ stl/minimum_number.py
@@ -0,0 +1,22 @@
+# User function Template for python3
+from functools import reduce
+from math import gcd
+
+
+class Solution:
+    def minimumNumber(self, n, arr):
+        # code here
+        return reduce(gcd, arr)
+
+# {
+ # Driver Code Starts
+# Initial Template for Python 3
+
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        n = int(input())
+        arr = [int(i) for i in input().split()]
+        obj = Solution()
+        print(obj.minimumNumber(n, arr))
+# } Driver Code Ends
