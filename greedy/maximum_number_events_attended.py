0418498e1f253b77a51281e08848cffc4ff1245b
huggin <1171705+huggin@users.noreply.github.com>
2023-04-23 00:16:08 -0700

    Maximum number of events that can be attended

---
+++ greedy/maximum_number_events_attended.py
@@ -0,0 +1,38 @@
+# User function Template for python3
+
+class Solution:
+    def maxEvents(self, start, end, N):
+        # code here
+
+        events = list(zip(start, end))
+        events.sort()
+        s = {}
+
+        for i in range(N-1, -1, -1):
+            if not events[i][1] in s:
+                s[events[i][1]] = 1
+            else:
+                e = events[i][1]
+                while e >= events[i][0]:
+                    if not e in s:
+                        s[e] = 1
+                        break
+                    e -= 1
+
+        return len(s)
+
+# {
+ # Driver Code Starts
+# Initial Template for Python 3
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        start = list(map(int, input().split()))
+        end = list(map(int, input().split()))
+
+        ob = Solution()
+        print(ob.maxEvents(start, end, N))
+# } Driver Code Ends
