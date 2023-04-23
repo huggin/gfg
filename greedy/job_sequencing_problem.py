867ea328aa4930e6d6761b05f38d4aa358a2668c
huggin <1171705+huggin@users.noreply.github.com>
2023-04-23 11:25:18 -0700

    N Digit numbers with digits in increasing order

--- greedy/job_sequencing_problem.py
+++ greedy/job_sequencing_problem.py
@@ -1,10 +1,14 @@
-#User function Template for python3
+# User function Template for python3
+import sys
+import io
+import atexit
 import heapq
 
+
 class Solution:
-    #Function to find the maximum profit and the number of jobs done.
-    def JobScheduling(self,Jobs,n):
-        
+    # Function to find the maximum profit and the number of jobs done.
+    def JobScheduling(self, Jobs, n):
+
         # code here
         Jobs.sort(key=lambda x: (x.deadline, -x.profit))
         pq = []
@@ -14,40 +18,41 @@ class Solution:
                     heapq.heapreplace(pq, j.profit)
             elif j.deadline > len(pq):
                 heapq.heappush(pq, j.profit)
-        
-        ans = [len(pq), sum(pq)]
-        
-        return ans
-        
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import atexit
-import io
-import sys
 
-#Contributed by : Nagendra Jha
+        ans = [len(pq), sum(pq)]
+
+        return ans
+
+
+# {
+ # Driver Code Starts
+# Initial Template for Python 3
+
+# Contributed by : Nagendra Jha
+
 class Job:
     '''
     Job class which stores profit and deadline.
     '''
-    def __init__(self,profit=0,deadline=0):
+
+    def __init__(self, profit=0, deadline=0):
         self.profit = profit
         self.deadline = deadline
         self.id = 0
 
+
 if __name__ == '__main__':
     test_cases = int(input())
-    for cases in range(test_cases) :
+    for cases in range(test_cases):
         n = int(input())
-        info = list(map(int,input().strip().split()))
+        info = list(map(int, input().strip().split()))
         Jobs = [Job() for i in range(n)]
         for i in range(n):
             Jobs[i].id = info[3*i]
             Jobs[i].deadline = info[3 * i + 1]
-            Jobs[i].profit=info[3*i+2]
+            Jobs[i].profit = info[3*i+2]
         ob = Solution()
-        res = ob.JobScheduling(Jobs,n)
-        print (res[0], end=" ")
-        print (res[1])
+        res = ob.JobScheduling(Jobs, n)
+        print(res[0], end=" ")
+        print(res[1])
 # } Driver Code Ends
---
+++ recursion/n_digit_numbers_increasing_order.py
@@ -0,0 +1,32 @@
+# User function Template for python3
+
+class Solution:
+    def solve(self, k, n, curr, prev=0):
+        if k == n:
+            self.ans.append(curr)
+            return
+
+        for i in range(prev+1, 10):
+            curr = curr * 10 + i
+            self.solve(k+1, n, curr, i)
+            curr = curr // 10
+
+    def increasingNumbers(self, N):
+        # code here
+        self.ans = [] if N != 1 else [0]
+        self.solve(0, N, 0)
+        return self.ans
+
+
+# {
+ # Driver Code Starts
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.increasingNumbers(N)
+        for num in ans:
+            print(num, end=' ')
+        print()
+# } Driver Code Ends
