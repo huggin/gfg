b18bf88bfd610cd68c76e3cac1df88a7858d8fbe
huggin <rwangth@gmail.com>
2023-04-26 09:48:51 -0700

    Top k numbers in a stream

--- hash/find_unique_pair_array_pairs_numbers.py
+++ hash/find_unique_pair_array_pairs_numbers.py
@@ -1,31 +1,32 @@
-#User function Template for python3
+# User function Template for python3
 from collections import Counter
 
+
 class Solution:
-    def findUniquePair(self, arr, n): 
-      
+    def findUniquePair(self, arr, n):
         # Complete the function
         c = Counter()
         for a in arr:
             c[a] += 1
-        
+
         ans = []
         for k, v in c.most_common():
             if v == 1:
                 ans.append(k)
-        
+
         if ans[0] > ans[1]:
             ans[0], ans[1] = ans[1], ans[0]
         return ans
 
-#{ 
- # Driver Code Starts
-    
-for _ in range(0,int(input())):
+
+# {
+# Driver Code Starts
+
+for _ in range(0, int(input())):
     n = int(input())
-    arr = list(map(int,input().strip().split()))
-    ob=Solution()
+    arr = list(map(int, input().strip().split()))
+    ob = Solution()
     v = ob.findUniquePair(arr, n)
     print(*v)
-    
+
 # } Driver Code Ends
--- hash/subarrays_sum_k.py
+++ hash/subarrays_sum_k.py
@@ -1,4 +1,5 @@
-#User function Template for python3
+# User function Template for python3
+
 
 class Solution:
     def findSubArraySum(self, Arr, N, k):
@@ -10,18 +11,19 @@ class Solution:
         for a in Arr:
             prefix += a
             if prefix - k in m:
-                ans += m[prefix-k]
+                ans += m[prefix - k]
             m[prefix] = m.get(prefix, 0) + 1
-        
+
         return ans
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
 
-if __name__ == '__main__':
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
     t = int(input())
-    for _ in range (t):
+    for _ in range(t):
         N = int(input())
         Arr = input().split()
         for itr in range(N):
