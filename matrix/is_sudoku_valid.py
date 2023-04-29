b4fcf05430d5bffefb09b1bddd7192ff3d35ab83
huggin <1171705+huggin@users.noreply.github.com>
2023-04-29 11:36:27 -0700

    Coins of Geekland

--- matrix/is_sudoku_valid.py
+++ matrix/is_sudoku_valid.py
@@ -1,4 +1,4 @@
-#User function Template for python3
+# User function Template for python3
 class Solution:
     def isValid(self, mat):
         # code here
@@ -10,7 +10,7 @@ class Solution:
                 c[mat[i][j]] += 1
                 if c[mat[i][j]] > 1:
                     return 0
-            
+
         for j in range(9):
             c = [0] * 10
             for i in range(9):
@@ -19,33 +19,34 @@ class Solution:
                 c[mat[i][j]] += 1
                 if c[mat[i][j]] > 1:
                     return 0
-                        
+
         for k in range(9):
             x = k // 3
             y = k % 3
             c = [0] * 10
-            for i in range(3*x, 3*x+3):
-                for j in range(3*y, 3*y+3):
+            for i in range(3 * x, 3 * x + 3):
+                for j in range(3 * y, 3 * y + 3):
                     if mat[i][j] == 0:
                         continue
                     c[mat[i][j]] += 1
                     if c[mat[i][j]] > 1:
                         return 0
-        
+
         return 1
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
 
-if __name__ == '__main__':
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
     t = int(input())
     for _ in range(t):
         arr = input().split()
-        mat = [[0]*9 for x in range(9)]
+        mat = [[0] * 9 for x in range(9)]
         for i in range(81):
-            mat[i//9][i%9] = int(arr[i])
-        
+            mat[i // 9][i % 9] = int(arr[i])
+
         ob = Solution()
         print(ob.isValid(mat))
 # } Driver Code Ends
--- matrix/rotate_90_degree.py
+++ matrix/rotate_90_degree.py
@@ -1,30 +1,32 @@
-#User function Template for python3
+# User function Template for python3
 
-def rotate(matrix): 
-    #code here
+
+def rotate(matrix):
+    # code here
     n = len(matrix)
-    matrix[:] = [[matrix[i][j] for i in range(n)] for j in range(n-1, -1, -1)]
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
+    matrix[:] = [[matrix[i][j] for i in range(n)] for j in range(n - 1, -1, -1)]
 
 
-if __name__ == '__main__':
-	t = int(input())
-	for _ in range(t):
-		N=int(input())
-		arr=[int(x) for x in input().split()]
-		matrix=[]
+# {
+# Driver Code Starts
+# Initial Template for Python 3
 
-		for i in range(0,N*N,N):
-			matrix.append(arr[i:i+N])
 
-		rotate(matrix)
-		for i in range(N): 
-			for j in range(N): 
-				print(matrix[i][j], end =' ') 
-			print() 
-        
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        arr = [int(x) for x in input().split()]
+        matrix = []
+
+        for i in range(0, N * N, N):
+            matrix.append(arr[i : i + N])
+
+        rotate(matrix)
+        for i in range(N):
+            for j in range(N):
+                print(matrix[i][j], end=" ")
+            print()
+
 
 # } Driver Code Ends
