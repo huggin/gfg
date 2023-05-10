943f9c05f48c56a5f3345fd03a4b08fcb34acac7
huggin <rwangth@gmail.com>
2023-05-10 10:08:18 -0700

    Let's Play!!!

---
+++ matrix/lets_play.py
@@ -0,0 +1,36 @@
+# User function Template for python3
+
+
+class Solution:
+    def isSuperSimilar(self, n, m, mat, x):
+        # code here
+        for i in range(n):
+            for j in range(m):
+                x %= m
+                if i % 2 == 0:
+                    if mat[i][j] != mat[i][(j + x) % m]:
+                        return 0
+                else:
+                    if mat[i][j] != mat[i][(j + m - x) % m]:
+                        return 0
+
+        return 1
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        n, m = [int(x) for x in input().split()]
+        mat = [[0] * m for x in range(n)]
+        arr = input().split()
+        for i in range(n * m):
+            mat[i // m][i % m] = int(arr[i])
+        x = int(input())
+
+        ob = Solution()
+        print(ob.isSuperSimilar(n, m, mat, x))
+# } Driver Code Ends
