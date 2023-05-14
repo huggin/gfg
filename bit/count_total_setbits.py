c62c5109254d7593222f33d387dff8aaa09d981e
huggin <1171705+huggin@users.noreply.github.com>
2023-05-14 16:32:00 -0700

    Check if a string is repetition of its substring of k-length

--- bit/count_total_setbits.py
+++ bit/count_total_setbits.py
@@ -5,13 +5,10 @@ class Solution:
     def countBits(self, N):
         # code here
         ans = 0
-        for i in range(20, 0, -1):
+        for i in range(31, 0, -1):
             if N & (1 << i):
-                # print(i, N)
                 ans += N % (1 << i) + 1 + (1 << i - 1) * i
-                # print(ans)
                 N &= (1 << i) - 1
-                # print(N)
         if N % 2:
             ans += 1
 
