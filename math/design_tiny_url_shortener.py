86ee34b517e9297fccacebc6be5742aa1a077bad
huggin <rwangth@gmail.com>
2023-05-15 20:18:28 -0700

    Missing Point of a Parallelogram

--- math/design_tiny_url_shortener.py
+++ math/design_tiny_url_shortener.py
@@ -1,40 +1,44 @@
-#User function Template for python3
+# User function Template for python3
+
 
 class Solution:
-    def idToShortURL(self,n):
+    def idToShortURL(self, n):
         # code here
         s = []
         while n:
             s.append(n % 62)
             n //= 62
-        
+
         for i in range(len(s)):
             if s[i] < 26:
-                s[i] = chr(s[i] + ord('a'))
+                s[i] = chr(s[i] + ord("a"))
             elif s[i] < 52:
-                s[i] = chr(s[i] - 26 + ord('A'))
+                s[i] = chr(s[i] - 26 + ord("A"))
             else:
-                s[i] = chr(s[i] - 52 + ord('0'))
-        
-        return ''.join(reversed(s))
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
+                s[i] = chr(s[i] - 52 + ord("0"))
+
+        return "".join(reversed(s))
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
 
 def shortURLToId(shortURL):
     id = 0
     for i in shortURL:
         val_i = ord(i)
-        if (val_i >= ord('a') and val_i <= ord('z')):
-            id = id * 62 + val_i - ord('a')
-        elif (val_i >= ord('A') and val_i <= ord('Z')):
-            id = id * 62 + val_i - ord('A') + 26
+        if val_i >= ord("a") and val_i <= ord("z"):
+            id = id * 62 + val_i - ord("a")
+        elif val_i >= ord("A") and val_i <= ord("Z"):
+            id = id * 62 + val_i - ord("A") + 26
         else:
-            id = id * 62 + val_i - ord('0') + 52
+            id = id * 62 + val_i - ord("0") + 52
     return id
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     tc = int(input())
     while tc > 0:
         n = int(input())
