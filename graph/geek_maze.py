38d1fc491858a1d4ccb03aed3c59b08e8a09c011
huggin <1171705+huggin@users.noreply.github.com>
2023-05-26 00:26:51 -0700

    Geek in a Maze

--- graph/geek_maze.py
+++ graph/geek_maze.py
@@ -1,65 +1,49 @@
 # User function Template for python3
+# Caution: Bug in test cases
 from collections import deque
 
 
-class Solution:
-    def numberOfCells(self, n, m, r, c, u, d, mat):
-        # code here
-        if mat[r][c] == "#":
-            return 0
+def numberOfCells(n, m, r, c, u, d, mat):
+    # code here
+    if mat[r][c] == "#":
+        return 0
 
-        marked = [[0 for _ in range(m)] for _ in range(n)]
-        marked[r][c] = 1
+    marked = [[0 for _ in range(m)] for _ in range(n)]
+    marked[r][c] = 1
 
-        q = deque()
-        q.append(r)
-        q.append(c)
-        q.append(u)
-        q.append(d)
+    q = deque()
+    q.append((r, c, u, d))
 
-        dx = [0, 0, -1, 1]
-        dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+    du = [0, 0, -1, 0]
+    dd = [0, 0, 0, -1]
 
-        ans = 0
-        while len(q):
-            x = q.popleft()
-            y = q.popleft()
-            if x == 1 and y == 2:
-                __import__('pdb').set_trace()
-            cu = q.popleft()
-            cd = q.popleft()
-            ans += 1
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if not (nx >= 0 and nx < n and ny >= 0 and ny < m):
-                    continue
-                if mat[nx][ny] == "#" or marked[nx][ny] == 1:
-                    continue
-                if i == 2 and cu == 0:
-                    continue
-                if i == 3 and cd == 0:
-                    continue
-                marked[nx][ny] = 1
-                q.append(nx)
-                q.append(ny)
-                if i == 2:
-                    q.append(cu - 1)
-                else:
-                    q.append(cu)
+    ans = 0
+    while len(q):
+        x, y, u, d = q.popleft()
+        ans += 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nu = u + du[i]
+            nd = d + dd[i]
+            if not (nx >= 0 and nx < n and ny >= 0 and ny < m and nu >= 0 and nd >= 0):
+                continue
+            if mat[nx][ny] == "#" or marked[nx][ny] == 1:
+                continue
 
-                if i == 3:
-                    q.append(cd - 1)
-                else:
-                    q.append(cd)
-        print(marked)
-        return ans
+            marked[nx][ny] = 1
+            if i == 0 or i == 1:
+                q.appendleft((nx, ny, nu, nd))
+            else:
+                q.append((nx, ny, nu, nd))
+    return ans
 
 
 # {
 # Driver Code Starts
 # Initial Template for Python 3
-
 
 if __name__ == "__main__":
     t = int(int(input()))
@@ -72,6 +56,6 @@ if __name__ == "__main__":
         for i in range(n):
             matele = [x for x in input()]
             mat.append(matele)
-        obj = Solution()
-        print(obj.numberOfCells(n, m, r, c, u, d, mat))
+
+        print(numberOfCells(n, m, r, c, u, d, mat))
 # } Driver Code Ends
