b0203fb7eda9df78d7b67e4e674a92ba08a564e9
huggin <rwangth@gmail.com>
2023-06-12 14:14:42 -0700

    k largest elements

---
+++ stl/k_largest_elements.py
@@ -0,0 +1,36 @@
+# User function Template for python3
+import heapq
+
+
+class Solution:
+    def kLargest(self, arr, n, k):
+        # code here
+        pq = []
+        for i in range(k):
+            heapq.heappush(pq, arr[i])
+
+        for i in range(k, n):
+            if arr[i] > pq[0]:
+                heapq.heapreplace(pq, arr[i])
+
+        return reversed(list(heapq.heappop(pq) for _ in range(k)))
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+
+if __name__ == "__main__":
+    tc = int(input())
+    while tc > 0:
+        n, k = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.kLargest(arr, n, k)
+        for x in ans:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
