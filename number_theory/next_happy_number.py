953aece178abb8d0f793f71d09c6f922d79d51f2
huggin <1171705+huggin@users.noreply.github.com>
2023-06-28 13:06:10 -0700

    Next Happy Number

--- number_theory/next_happy_number.py
+++ number_theory/next_happy_number.py
@@ -1,26 +1,29 @@
 # User function Template for python3
+from functools import lru_cache
 
 
 class Solution:
-    def solve(self, n):
-        if n == 1 or n == 7:
-            return True
-        elif n <= 9:
-            return False
-
-        ans = 0
-        while n:
-            d = n % 10
-            ans += d * d
-            n //= 10
-
-        return self.solve(ans)
-
     def nextHappy(self, N):
         # code here
+
+        @lru_cache(maxsize=None)
+        def f(n):
+            if n == 1 or n == 7:
+                return True
+            elif n <= 9:
+                return False
+
+            ans = 0
+            while n:
+                d = n % 10
+                ans += d * d
+                n //= 10
+
+            return f(ans)
+
         n = N + 1
         while True:
-            if self.solve(n):
+            if f(n):
                 return n
             n += 1
         return -1
