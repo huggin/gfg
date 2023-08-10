34d0528ee6dddd87086f335bb0c232997d01d01a
huggin <1171705+huggin@users.noreply.github.com>
2023-08-10 12:38:28 -0700

    Coin Change

---
+++ dp/coin_change.py
@@ -0,0 +1,35 @@
+# User function Template for python3
+
+
+class Solution:
+    def count(self, coins, N, Sum):
+        # code here
+        # coins.sort()
+        dp = [0] * (Sum + 1)
+        dp[0] = 1
+
+        for i in range(N):
+            for j in range(Sum + 1):
+                if j >= coins[i]:
+                    dp[j] += dp[j - coins[i]]
+
+        return dp[Sum]
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+import sys
+
+sys.setrecursionlimit(10**6)
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        sum, N = list(map(int, input().strip().split()))
+        coins = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.count(coins, N, sum))
+
+# } Driver Code Ends
