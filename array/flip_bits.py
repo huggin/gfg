8e0078d125f79be51fe186f2f91d8a848477bc7d
huggin <1171705+huggin@users.noreply.github.com>
2023-08-14 13:01:36 -0700

    Flip Bits

--- array/flip_bits.py
+++ array/flip_bits.py
@@ -4,38 +4,18 @@
 class Solution:
     def maxOnes(self, a, n):
         # Your code goes here
-        if a.count(1) == n:
-            return n
-
-        prefix = [0] * n
-        suffix = [0] * n
-        if a[0] == 1:
-            prefix[0] = 1
-        for i in range(1, n):
-            prefix[i] += prefix[i - 1] + a[i]
-
-        if a[n - 1] == 1:
-            suffix[n - 1] = 1
-        for i in range(n - 2, -1, -1):
-            suffix[i] += suffix[i + 1] + a[i]
-
         ans = 0
-        j = -1
-        tt = 0
-        for i in range(n):
-            if a[i] == 1:
-                tt += 1
+        zero, one = 0, 0
+        for c in a:
+            if c == 0:
+                zero += 1
             else:
-                if tt >= 0:
-                    tt = -1
-                    j = i
-                else:
-                    tt -= 1
-
-                if j != -1:
-                    ans = max(ans, -tt + prefix[i] + suffix[i])
-
-        return ans
+                one += 1
+                zero -= 1
+            ans = max(ans, zero)
+            if zero < 0:
+                zero = 0
+        return ans + one
 
 
 # {
