ff499e0609dff9ccd31a04da930c92a9ec9125b0
huggin <rwangth@gmail.com>
2023-09-17 12:18:49 -0700

    Power of 2

---
+++ math/power_2.py
@@ -0,0 +1,40 @@
+# User function Template for python3
+
+
+class Solution:
+    ##Complete this function
+    # Function to check if given number n is a power of two.
+    def isPowerofTwo(self, n):
+        ##Your code here
+        if n == 0:
+            return False
+        a = int(math.log2(n))
+        if 2**a == n:
+            return True
+        return False
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+
+    while T > 0:
+        n = int(input())
+        ob = Solution()
+        if ob.isPowerofTwo(n):
+            print("YES")
+        else:
+            print("NO")
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
