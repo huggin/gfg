f92e9bccb25138ff9ce0027aca8a4bd8b23ae39b
huggin <1171705+huggin@users.noreply.github.com>
2024-04-09 22:43:36 -0700

    Pairs violating the BST property

---
+++ segment_tree/pairs_violating_bst_property.py
@@ -0,0 +1,156 @@
+from typing import Optional
+from collections import deque
+import bisect
+
+"""
+
+definition of binary tree node.
+class Node:
+    def _init_(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+"""
+
+
+class Solution:
+    def pairsViolatingBST(self, n: int, root: Optional["Node"]) -> int:
+        # code here
+        a = []
+
+        def f(p):
+            if p is None:
+                return
+            f(p.left)
+            a.append(p.data)
+            f(p.right)
+
+        f(root)
+        ans = 0
+        b = sorted(a)
+        n = len(a)
+        for i in range(n):
+            a[i] = bisect.bisect_left(b, a[i])
+
+        fenwick = [0] * (n + 1)
+
+        def add(i):
+            i += 1
+            while i <= n:
+                fenwick[i] += 1
+                i += i & -i
+
+        def get(i):
+            i += 1
+            ans = 0
+            while i > 0:
+                ans += fenwick[i]
+                i -= i & -i
+            return ans
+
+        ans = 0
+        for i in range(n - 1, -1, -1):
+            ans += get(a[i] - 1)
+            add(a[i])
+
+        return ans
+
+
+# {
+# Driver Code Starts
+
+
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.right = None
+        self.left = None
+
+
+# Function to Build Tree
+def buildTree(s):
+    # Corner Case
+    if len(s) == 0 or s[0] == "N":
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while size > 0 and i < len(ip):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if currVal != "N":
+
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if i >= len(ip):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if currVal != "N":
+
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+
+def inputTree():
+    treeString = input().strip()
+    root = buildTree(treeString)
+    return root
+
+
+def inorder(root):
+    if root == None:
+        return
+    inorder(root.left)
+    print(root.data, end=" ")
+    inorder(root.right)
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        root = inputTree()
+
+        obj = Solution()
+        res = obj.pairsViolatingBST(n, root)
+
+        print(res)
+
+
+# } Driver Code Ends
